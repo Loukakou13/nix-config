313a8696a1ce988452f69034cb2fe0c2a01674bc
Louca Guermeur <louca.guermeur@epita.fr>
2026-05-04 22:11:50 +0200

    feat: added graphic drivers for laptop host

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -49,7 +49,24 @@
     LC_TIME = "fr_FR.UTF-8";
   };
 
-  hardware.graphics.enable = true;
+  # GPU configurations for intel iris Xe
+  services.xserver.videoDrivers = [ "modesetting" ];
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      # Required for modern Intel GPUs (Xe iGPU and ARC)
+      intel-media-driver     # VA-API (iHD) userspace
+      vpl-gpu-rt             # oneVPL (QSV) runtime
+
+      # Optional (compute / tooling):
+      intel-compute-runtime  # OpenCL (NEO) + Level Zero for Arc/Xe
+    ];
+  };
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";     # Prefer the modern iHD backend
+  };
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
