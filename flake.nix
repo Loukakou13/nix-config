9237ba51b06bd7cd5269e2c9749491883757e0d9
Louca Guermeur <louca.guermeur@epita.fr>
2026-05-06 19:25:40 +0200

    feat: added home-manager

--- flake.nix
+++ flake.nix
@@ -2,6 +2,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@attrs: {
